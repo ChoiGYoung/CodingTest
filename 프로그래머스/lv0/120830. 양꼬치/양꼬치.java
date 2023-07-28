34bacf272228901d8703f215718da9fc5ea9b322
ChoiGYoung <110095557+ChoiGYoung@users.noreply.github.com>
2023-07-29 00:35:21 +0900

    [level 0] Title: 양꼬치, Time: 0.02 ms, Memory: 75.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120830. 양꼬치/양꼬치.java
+++ 프로그래머스/lv0/120830. 양꼬치/양꼬치.java
@@ -1,10 +1,8 @@
 class Solution {
     public int solution(int n, int k) {
         int answer = 0;
-        if(n>9){
-            k = k - n/10;
-        }
-        answer= 12000*n + 2000*k;
+        
+        answer= 12000*n + 2000*k - n/10*2000;
         return answer;
     }
 }
